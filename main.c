48607ec2759f025f6e19a9485e72ab2d0fe2bf5d
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-12-24 00:03:07 -0500

    f

--- main.c
+++ main.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
-#include <time.h>
+#include <windows.h>
 
-#define WINDOW_SIZE 160  // Size of the sliding window (number of samples)
+#define WINDOW_SIZE 160  // Size of the sliding window 
+#define TARGET_CALLS 1000000  // Number of calls to measure
+
+int screen_buffer[WINDOW_SIZE] = {0};
+volatile long call_count = 0;
+
+void CALLBACK high_frequency_function(PVOID lpParameter, BOOLEAN TimerOrWaitFired) {
+    // Simple operation to keep the function lightweight
+    screen_buffer[0]++;
+    InterlockedIncrement(&call_count);
+}
 
 int main() {
-    // Parameters
-    int window_size = WINDOW_SIZE;  // Size of the sliding window (number of samples)
-    int sampling_rate = 1000;  // Sample rate (samples per second)
-    float change_threshold = 0.5;  // Threshold for detecting sudden changes
+    HANDLE hTimer = NULL;
+    LARGE_INTEGER liDueTime;
+    liDueTime.QuadPart = -1; // Start immediately
 
-    // memory
-    float screen_buffer[WINDOW_SIZE] = {0};
+    LARGE_INTEGER frequency, start, end;
+    QueryPerformanceFrequency(&frequency);
 
-    int u = 0;
-    int i = 0;
-    int index = 0;
-
-    // Start timer
-    clock_t start_time = clock();
-
-    while (i < 10900900) {
-        i += 1;
-        u = i + 5;
-        u = u * 9;
-        u = u / 2;
-        u = u - 1;
-        index += 1;
-        screen_buffer[index] = u;
-        if (index == window_size - 1) {
-            index = 0;
-        }
+    // Create a high-resolution timer
+    if (!CreateTimerQueueTimer(&hTimer, NULL, (WAITORTIMERCALLBACK)high_frequency_function, NULL, 0, 1, WT_EXECUTEINTIMERTHREAD)) {
+        printf("CreateTimerQueueTimer failed (%d)\n", GetLastError());
+        return 1;
     }
 
-    // End timer
-    clock_t end_time = clock();
-    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    // Measure the time taken for TARGET_CALLS calls
+    QueryPerformanceCounter(&start);
+    while (call_count < TARGET_CALLS) {
+        // Do nothing, just wait for the timer callback to be called
+    }
+    QueryPerformanceCounter(&end);
 
-    printf("done\n");
-    printf("Time taken: %f seconds\n", time_taken);
+    // Calculate the elapsed time in seconds
+    double elapsed_time = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+    printf("Time taken for %d calls: %f seconds\n", TARGET_CALLS, elapsed_time);
+
+    // Clean up the timer
+    DeleteTimerQueueTimer(NULL, hTimer, NULL);
 
     return 0;
-}
+}f
